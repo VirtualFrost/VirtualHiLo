efc9e913a78240366a205e24b658b1b889e1d232
VirtualFrost <virtuallfrost@gmail.com>
2016-08-31 18:33:15 +1000

    Added a check to make sure the input is between 1 and 100. Fixed issue with crashing when it wasn't an integer.

--- VirtualHiLo/VirtualHiLo/Game.cs
+++ VirtualHiLo/VirtualHiLo/Game.cs
@@ -7,13 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+    /* To Do:
+     * Add highscores
+     * Difficulties
+     *      - Easy = 10 Guesses
+     *      - Normal = 7 Guesses
+     *      - Hard = 3 Guesses
+     *      - Expert = 1 Guess
+     * Check that only integers are being inputted
+     */
 namespace VirtualHiLo
 {
     public partial class virtualHiLo : Form
     {
         int userGuess = 0;
         int randNumber;
+        
         public virtualHiLo()
         {
             InitializeComponent();
@@ -22,29 +31,42 @@ namespace VirtualHiLo
 
         private void guessBtn_Click(object sender, EventArgs e)
         {
-            int guessedNumber = Convert.ToInt16(userInputTextBox.Text);
+            string n = userInputTextBox.Text;
+            int guessedNumber;
             guessesLbl.Text = Convert.ToString(7 - userGuess);
-            if (userGuess != 7) //8 Guesses
+
+            if (int.TryParse(n, out guessedNumber))             //Checks to make sure input is integer
             {
-                if (guessedNumber > randNumber)
+                if (guessedNumber > 101 || guessedNumber < 0)
                 {
-                    resultTextBox.AppendText("Too high!\n");
+                    resultTextBox.AppendText("You must enter a value between 1 and 100.");
                 }
-                else if (guessedNumber < randNumber)
+                if (userGuess != 9) //10 Guesses
                 {
-                    resultTextBox.AppendText("Too low!\n");
+                    if (guessedNumber > randNumber)
+                    {
+                        resultTextBox.AppendText("Too high!\n");
+                    }
+                    else if (guessedNumber < randNumber)
+                    {
+                        resultTextBox.AppendText("Too low!\n");
+                    }
+                    else
+                    {
+                        guessBtn.Visible = false;
+                        resultTextBox.AppendText("Correct!");
+                    }
+                    userGuess++;
                 }
                 else
                 {
                     guessBtn.Visible = false;
-                    resultTextBox.AppendText("Correct!");
+                    resultTextBox.AppendText("You have ran out of guesses, the answer is: " + randNumber + "!");
                 }
             } else
             {
-                guessBtn.Visible = false;
-                resultTextBox.AppendText("You have ran out of guesses, the answer is: " + randNumber + "!");
+                resultTextBox.AppendText("You must enter a value between 1 and 100.");
             }
-            userGuess++;
         }
 
         private void newGameBtn_Click(object sender, EventArgs e)
